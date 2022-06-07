f95a180b8a2bd9de7ad3066727a47ac47b7694e0
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-07 23:18:15 +0600

    next js home page body background and basics ui created

---
+++ blockchain-server/schemas/userSchema.js
@@ -0,0 +1,22 @@
+export const userSchema = {
+    name: "users",
+    type: "document",
+    title: "Users",
+    fields: [
+        {
+            name: "name",
+            type: "string",
+            title: "Name",
+        },
+        {
+            name: "wallerAddress",
+            type: "string",
+            title: "Watller Address",
+        },
+        {
+            name: "profileImage",
+            type: "image",
+            title: "Profile Image",
+        },
+    ],
+};
